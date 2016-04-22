1a5e42672502ec763c4b179b4ee732feeca460e8
SKutukov <skutukov96@gmail.com>
2016-04-22 23:04:46 +0300

    add procedure

---
+++ 1.procedure.sql
@@ -0,0 +1,14 @@
+use BankDataBase
+create proc MoneyMove(
+@amout int,
+@ac_id_out int,
+@ac_id_in int
+)as 
+update  AccountList set money=money-@amout  where id=@ac_id_out; 
+update  AccountList set money=money+@amout  where id=@ac_id_in;
+
+insert into OperationLog values (@ac_id_in,'пополнение',@amout,GETDATE()); 
+insert into OperationLog values (@ac_id_out,'списание',@amout,GETDATE()); 
+
+
+exec MoneyMove @amout=10,@ac_id_out=1,@ac_id_in=2 
